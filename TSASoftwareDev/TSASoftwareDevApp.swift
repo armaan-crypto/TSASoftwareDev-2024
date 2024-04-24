54ccccd1cbdc4732032064c9c1d07731a583ceb3
armaan-crypto <armaanahmed08@gmail.com>
2024-04-24 15:38:29 -0400

    Initial Commit

---
+++ TSASoftwareDev/TSASoftwareDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  TSASoftwareDevApp.swift
+//  TSASoftwareDev
+//
+//  Created by Armaan Ahmed on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct TSASoftwareDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
